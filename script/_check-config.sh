bdc849b6b3d58717a867c0961cc3bf37dd067ad0
Pedro Trujillo <pedroetb@gmail.com>
2020-11-10 14:18:13 +0000

    Fix config check when using multiple compose files

--- script/_check-config.sh
+++ script/_check-config.sh
@@ -3,7 +3,17 @@
 echo -e "\n${INFO_COLOR}Checking deployment configuration in docker-compose files ..${NULL_COLOR}"
 echo -e "  ${INFO_COLOR}compose files [ ${DATA_COLOR}${composeFilePath}${INFO_COLOR} ]${NULL_COLOR}\n"
 
-if [ ! -f "${composeFilePath}" ]
+anyComposeMissing=0
+for composeFilePathItem in $(echo ${composeFilePath} | tr ':' ' ')
+do
+	if [ ! -f "${composeFilePathItem}" ]
+	then
+		echo -e "${DATA_COLOR}${composeFilePathItem}${INFO_COLOR} not found.${NULL_COLOR}"
+		anyComposeMissing=1
+	fi
+done
+
+if [ "${anyComposeMissing}" -eq 1 ]
 then
 	echo -e "${INFO_COLOR}Docker-compose configuration not found, omitting check ..${NULL_COLOR}"
 	FORCE_DOCKER_BUILD=1
