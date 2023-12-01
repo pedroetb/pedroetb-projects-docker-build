911b32312dfd59e1bb2387d887016aac1170fb08
Pedro Trujillo <pedroetb@gmail.com>
2023-12-01 16:20:43 +0000

    Add initial SSH connection check

--- script/_ssh-config.sh
+++ script/_ssh-config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-remoteHost=$(echo "${SSH_BUILD_REMOTE}" | cut -f 2 -d '@')
+remoteUser=$(echo "${SSH_BUILD_REMOTE}" | cut -d '@' -f 1)
+remoteHost=$(echo "${SSH_BUILD_REMOTE}" | cut -d '@' -f 2)
 
 if [ -z "${remoteHost}" ]
 then
-	echo -e "${FAIL_COLOR}You must define 'SSH_BUILD_REMOTE' in environment, with remote user and hostname (like 'ssh-user@ssh-remote')${NULL_COLOR}"
+	echo -e "${FAIL_COLOR}Remote host not found, define 'SSH_BUILD_REMOTE' with remote user and hostname (like 'ssh-user@ssh-remote')${NULL_COLOR}"
 	exit 1
 fi
 
@@ -18,4 +19,16 @@ fi
 eval "$(ssh-agent)" > /dev/null
 echo "${SSH_BUILD_KEY}" | tr -d '\r' | ssh-add - > /dev/null 2>&1
 
-closeSshCmd="ssh ${SSH_PARAMS} -q -O exit \"${SSH_BUILD_REMOTE}\""
+closeSshCmd="ssh -l ${remoteUser} ${SSH_PARAMS} -q -O exit \"${remoteHost}\""
+
+runRemoteCmd() {
+	ssh -l ${remoteUser} ${SSH_PARAMS} "${remoteHost}" "${1}"
+}
+
+# Check remote connectivity
+if ! runRemoteCmd ":" &> /dev/null
+then
+	echo -e "\n${FAIL_COLOR}Failed to connect to host ${DATA_COLOR}${remoteHost}${FAIL_COLOR} at port ${DATA_COLOR}${SSH_BUILD_PORT}${FAIL_COLOR} with user ${DATA_COLOR}${remoteUser}${FAIL_COLOR}!${NULL_COLOR}"
+	eval "${closeSshCmd}"
+	exit 1
+fi
